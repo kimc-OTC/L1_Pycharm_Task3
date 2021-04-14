1278957cbbccd242c197b178d15cf38f776aa68d
kimc-OTC <kimc@stu.otc.school.nz>
2021-04-15 09:40:14 +1200

    Trying to find out a solution for task 2 Q3

--- Name_List.py
+++ Name_List.py
@@ -8,7 +8,14 @@ if user_name in name_list:
 else:
     print("Sorry, {} is not in my list of names.".format(user_name))
 
-replace_or_add = input("Would you like to replace a name in the list with your name OR would you like to just add your name into the list?").strip().capitalize()
+replace_or_add = input("Would you like to 'replace' a name in the list with your name OR would you like to just 'add' your name to the list?").strip().capitalize()
 if replace_or_add == "Replace":
-    replaced_list = []
-    print("Here is the replaced list: {}".format())
+    replacement_name = input("Type in a name that you want to replace it with: \n"
+                             "{}".format(name_list)).strip().capitalize()
+    name_position = name_list.index(replacement_name)
+    replaced_list = name_list + name_list[name_position].replace(replacement_name)
+
+
+
+#replaced_list = []
+#print("Here is the replaced list: {}".format())
