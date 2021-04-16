14d18db54f855e1315fa24f4cf27beac517f83bb
kimc-OTC <kimc@stu.otc.school.nz>
2021-04-16 13:31:04 +1200

    Finished all tasks and added a feature that asks  the user for a proper answer until they do.

--- Name_List.py
+++ Name_List.py
@@ -8,14 +8,24 @@ if user_name in name_list:
 else:
     print("Sorry, {} is not in my list of names.".format(user_name))
 
-replace_or_add = input("Would you like to 'replace' a name in the list with your name OR would you like to just 'add' your name to the list?").strip().capitalize()
-if replace_or_add == "Replace":
-    replacement_name = input("Type in a name that you want to replace it with: \n"
-                             "{}".format(name_list)).strip().capitalize()
-    name_position = name_list.index(replacement_name)
-    replaced_list = name_list + name_list[name_position].replace(replacement_name)
+repeat = True
+
+while repeat == True:
+    replace_or_add = input("Would you like to 'replace' a name in the list with your name OR would you like to just 'add' your name to the list?").strip().capitalize()
+    if replace_or_add == "Replace":
+        replacement_name = input("Type in a name that you want to replace it with: \n"
+                                 "{}".format(name_list)).strip().capitalize()
+        name_position = name_list.index(replacement_name)
+        name_list[name_position] = user_name
+        print("Here is the replaced list: {}".format(name_list))
+        repeat = False
+    elif replace_or_add == "Add":
+        name_list.append(user_name)
+        print("Here is the added list: {}".format(name_list))
+        repeat = False
+    else:
+        print("Please type in either 'replace' OR 'add'.")
 
 
 
-#replaced_list = []
-#print("Here is the replaced list: {}".format())
+
