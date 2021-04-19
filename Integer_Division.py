869beea29a4821c51c61576f3f88620ec7a7e8eb
kimc-OTC <kimc@stu.otc.school.nz>
2021-04-19 20:41:44 +1200

    Added comments.

--- Integer_Division.py
+++ Integer_Division.py
@@ -1,10 +1,13 @@
+# set variables
 chocolate_bar = 18
 people = 5
 
+# calculate each of the following equations
 whole_bar = chocolate_bar // people
 extra_squares = whole_bar * 7 // people
 leftover = whole_bar * 7 % people
 
+# print the outcomes of each equations
 print("Whole chocolate bars each: {}\n"
       "Extra squares each: {}\n"
       "Squares left over: {}\n".format(whole_bar, extra_squares, leftover))
